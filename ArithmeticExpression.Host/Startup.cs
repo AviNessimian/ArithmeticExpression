d437d6e348de586d95fe6390637ecc26efd12b57
avi nessimian <nessimian.avi@outlook.com>
2023-09-15 04:06:57 +0300

    s

--- ArithmeticExpression.Host/Startup.cs
+++ ArithmeticExpression.Host/Startup.cs
@@ -4,10 +4,12 @@ namespace ArithmeticExpression.Host;
 public class Startup
 {
     private readonly IConfiguration _configuration;
+    private readonly string[] _allowedCorsOrigins;
 
     public Startup(IConfiguration configuration)
     {
         _configuration = configuration;
+        _allowedCorsOrigins = _configuration.GetSection("AllowedCorsOrigins").Get<string[]>();
     }
 
     public void ConfigureServices(IServiceCollection services)
@@ -15,13 +17,12 @@ public class Startup
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
 
-        var allowedCorsOrigins = _configuration.GetSection("AllowedCorsOrigins").Get<string[]>();
-        if (allowedCorsOrigins?.Any() ?? false)
+        if (_allowedCorsOrigins?.Any() ?? false)
         {
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
-                    builder => builder.WithOrigins(allowedCorsOrigins)
+                    builder => builder.WithOrigins(_allowedCorsOrigins)
                         .AllowAnyHeader()
                         .AllowAnyMethod());
             });
@@ -41,7 +42,10 @@ public class Startup
 
         app.UseStaticFiles();
         app.UseRouting();
-        app.UseCors("CorsPolicy");
+        if (_allowedCorsOrigins?.Any() ?? false)
+        {
+            app.UseCors("CorsPolicy");
+        }
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapCalculatorRoute();
